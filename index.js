d73b423cf1c7b1176cbc81cf7e08caf2783f7db3
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 23:24:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function produceDrivingRange(drivingRange) {
   return function(arg1, arg2){
     const x = Math.abs(parseInt(arg2) - parseInt(arg1))
-    
+    if (x >= drivingRange) {
+      return `within range by ${x}`
+  } else {
+    return `${x} blocks out of range`
+  }
 }
 }
