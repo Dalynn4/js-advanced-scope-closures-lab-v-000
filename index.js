5e98da90a370b915394188286a31271abde02650
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 23:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(drivingRange) {
   return function(arg1, arg2){
-    const x = Math.abs(arg2 - arg1)
-    return parseInt(arg1)
+    const x = Math.abs(parseInt(arg2) - parseInt(arg1))
+    
 }
 }
