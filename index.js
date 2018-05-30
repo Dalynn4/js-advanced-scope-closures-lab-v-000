8c4a5225210956ddb5380e010eb50a53cc5d132c
Randall Robinson <keith_adams007@yahoo.com>
2018-05-30 23:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function produceDrivingRange(drivingRange) {
   return function(arg1, arg2){
-    if (arg2 - arg1 >= drivingRange) {
-      return 
-    }
+    const x = arg2 - arg1
+    if (x >= drivingRange) {
+      return `
   }
+}
 }
